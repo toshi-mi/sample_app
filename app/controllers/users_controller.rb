7fed75d7f4e69211966ee37b22736a5783f9a203
toshi443 <toshi.443.aki@gmail.com>
2016-04-29 08:26:57 +0000

    Finish upgrade

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,14 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:index, :edit, :update, :show, :destroy]
+  before_action :admin_user, only: :destroy
+  
+  
   def new
     @user = User.new
+  end
+  
+  def index
+    @users = User.all
   end
   
   def show
@@ -17,8 +25,37 @@ class UsersController < ApplicationController
     end
   end
   
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to users_path
+  end
+  
   private
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+    
+     def logged_in_user
+      unless logged_in?
+        redirect_to login_url
+      end
+     end
+     
+    def admin_user
+      redirect_to root_path unless current_user.admin?
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160429035834) do
+ActiveRecord::Schema.define(version: 20160429080704) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
     t.string   "password_digest"
+    t.boolean  "admin",           default: false
   end
 
 end
